bdc9423230d230cedda321219ab30e59ff28d6d3
joaquincruzat <joaquincruzat@gmail.com>
2022-05-18 22:09:57 -0400

    primer commit

---
+++ src/components/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h1>Bienvenidos a Cobra Kai!</h1>
+  <img src="https://logos-marcas.com/wp-content/uploads/2021/08/Cobra-Kai-Logo-650x366.png" alt="">
+ </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/Results.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>¡Peleador Encontrado!</h1>
+    <div>
+      <p>{{peleador.id}}</p>
+      <p>{{peleador.nombre}}</p>
+      <img :src="peleador.imgSrc" alt="">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data: ()=>({
+   peleador:""
+  }), 
+  created() {
+    const idPeleador= this.$route.params.id
+    fetch("/peleadores.json")
+      .then((res) => res.json())
+      .then((data) => {
+        this.peleador = data.find((item)=> (
+          item.id == idPeleador
+        ));
+      });
+  },
+}
+</script>
